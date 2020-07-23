d27604099ced2f4fb8b5c69607323e9b689f5160
bangden07 <indogocloud@gmail.com>
2020-07-23 21:50:18 +0700

    modif footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -36,7 +36,7 @@ export default function Footer() {
           </a>
         </nav>
         <nav className="flex">
-          <a
+          Built with <a
             href="https://www.gatsbyjs.org/"
             title="Built with Gatsby"
             target="_blank"
@@ -44,24 +44,6 @@ export default function Footer() {
             className="img"
           >
             <img src={gatsby} className="footer-img" alt="Gatsby" />
-          </a>
-          <a
-            href="https://github.com/taniarascia"
-            title="Open-source on GitHub"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="img"
-          >
-            <img src={github} className="footer-img" alt="GitHub" />
-          </a>
-          <a
-            href="https://www.netlify.com/"
-            title="Hosted by Netlify"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="img"
-          >
-            <img src={netlify} className="footer-img" alt="Netlify" />
           </a>
         </nav>
       </section>
